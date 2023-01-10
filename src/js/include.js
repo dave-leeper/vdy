f12d7e0f3698f60b4493d7ee0f73529bd9bbbef8
David Leeper <magicjtv@gmail.com>
2023-01-10 07:28:49 -0700

    Added some comments to explain custom tags

--- src/js/include.js
+++ src/js/include.js
@@ -756,46 +756,43 @@ class Component {
 }
 
 class SlotManager {
-    static getAllSlotMarkup() {
-        return document.querySelectorAll('slot-markup')
-    }
-    static getSlot(forComponentId, slotName) {
-        const componentElement = document.getElementById(forComponentId)
-
-        if (!componentElement) {
-            console.error(`getSlot: Component element ${forComponentId} was not found.`)
-            return null
-        }
-
-        const componentSlot = componentElement.querySelector(`component-slot[id=${slotName}]`)
-
-        if (!componentSlot) {
-            console.error(`getSlot: Component slot ${slotName} was not found.`)
-            return null
-        }
-
-        return componentSlot
-    }
-    static moveSlotContentToComponent(slotContentElement, componentSlotElement) {
-        if (0 === slotContentElement.children.length) {
-            console.error(`moveSlotContentToComponent: Slot content element has no children.`)
-            return false
-        }
-        if (0 !== componentSlotElement.children.length) {
-            console.error(`moveSlotContentToComponent: Component slot ${componentElement.getAttribute(`for-slot`)} was not found.`)
-            return false
-        }
-
-        while (0 < slotContentElement.children.length) {
-            componentSlotElement.after(slotContentElement.firstChild)
-        }
-        componentSlotElement.remove()
-        slotContentElement.remove()
-
-        return true
-    }
     static loadSlots() {
-        const slotMarkupElements = SlotManager.getAllSlotMarkup()
+        const getSlot = (forComponentId, slotName) => {
+            const componentElement = document.getElementById(forComponentId)
+    
+            if (!componentElement) {
+                console.error(`getSlot: Component element ${forComponentId} was not found.`)
+                return null
+            }
+    
+            const componentSlot = componentElement.querySelector(`component-slot[id=${slotName}]`)
+    
+            if (!componentSlot) {
+                console.error(`getSlot: Component slot ${slotName} was not found.`)
+                return null
+            }
+    
+            return componentSlot
+        }
+        const moveSlotContentToComponent = (slotContentElement, componentSlotElement) => {
+            if (0 === slotContentElement.children.length) {
+                console.error(`moveSlotContentToComponent: Slot content element has no children.`)
+                return false
+            }
+            if (0 !== componentSlotElement.children.length) {
+                console.error(`moveSlotContentToComponent: Component slot ${componentElement.getAttribute(`for-slot`)} was not found.`)
+                return false
+            }
+    
+            while (0 < slotContentElement.children.length) {
+                componentSlotElement.after(slotContentElement.firstChild)
+            }
+            componentSlotElement.remove()
+            slotContentElement.remove()
+    
+            return true
+        }
+        const slotMarkupElements = document.querySelectorAll('slot-markup')
 
         for (let slotContent of slotMarkupElements) {
             const forComponentId = slotContent.getAttribute(`for-component-id`)
@@ -806,10 +803,10 @@ class SlotManager {
             }
 
             const slotName = slotContent.getAttribute(`for-slot`)
-            const componentSlot = SlotManager.getSlot(forComponentId, slotName)
+            const componentSlot = getSlot(forComponentId, slotName)
 
             if (!componentSlot) { continue }
-            if (!SlotManager.moveSlotContentToComponent(slotContent, componentSlot)) {
+            if (!moveSlotContentToComponent(slotContent, componentSlot)) {
                 console.error(`loadSlots: An error occured while moving slot content to the ${componentElement.getAttribute(`for-slot`)}] slot of ${forComponentId}.`)
                 continue
             }
@@ -966,21 +963,64 @@ class Loader {
         await Loader.loadIncludes()
     }
     static registerCustomTags = function () {
-        customElements.define('custom-component', class VanillaComponentElement extends HTMLElement { }, { })
-        customElements.define('test-script', class TestScriptElement extends HTMLElement { }, { })
-        customElements.define('component-markup', class ComponentMarkupElement extends HTMLElement { }, { })
+        // * The include-html tag can include a file containing HTML into another HTML document. The HTML to be
+        // included can be a normal HTML snippet, or can be an HTML component. 
+        // * In both cases the include-in attribute is used to indicate the name of the file receiving the HTML and 
+        // the src attribute is used to provide the location of the file to be included.
+        // * When including an HTML component, the include-html tag must also have a component-class attribute
+        // indicating the class of the component and a component-id attribute providing an id for the component instance.
+        // * An object of the type indicated by the component-class attrubute which represents the component instance is
+        // created and can be accessed using the Component.getObject(id) method, where id is the value of the component-id
+        // attribute of the include-html tag.
+        // * The HTML DOM fragment used to create instance of the component can be accessed using the 
+        // Component.getFragment(className) method, where className is the value of the component-class attribute of the
+        // include-html tag.
+        // * It is standard practice to give the root element of the component the same id as the component's class. This
+        // makes it easy to locate the DOM element for the component.
+        // The object represent an instance of a component has an id member, myComponent.id, that has the value provided by
+        // the component-id attribute of the include-html.
+        // The value of the id member of the object representing the component can be accessed in the HTML markup of the 
+        // component in attributes or in innerText using the {id} syntax. Example: id="{id}".
         customElements.define('include-html', class IncludeHTMLElement extends HTMLElement { }, { })
+
+        // * Components can have props, which are values that cannot be changed after the component has been initialized. The
+        // values for the props of an instance of a component can be set using the include-props tag. The inner text of this
+        // tag contans JSON used to inialize the props.
+        // * The include-props tag can only appear inside the include-html tag of a component. 
         customElements.define('include-props', class IncludePropsElement extends HTMLElement { }, { })
+
+        // * Components can have vars, which are values that can be changed throughout the lifetime of the component object. The
+        // values for the vars of an instance of a component can be set using the include-vars tag. The inner text of this
+        // tag contans JSON used to inialize the vars.
+        // * The include-vars tag can only appear inside the include-html tag of a component. 
         customElements.define('include-vars', class IncludeVarsElement extends HTMLElement { }, { })
-        // TO DO: Add support for the following tags
+
+        // * Defines a custom component. Custom components must contain:
+        //      * A script tag which contains the javascript for a class that extends the Component class. The script
+        //       tag may optionally contain additonal javascript.
+        //      * A component-markup that provides the HTML markup for the component.
+        // * A custom-component tag may also contain:
+        //      * A test-script tag that contains testing code for the tag.
+        //      * A style tag that contains CSS. This is usually CSS used by the component.
+        customElements.define('custom-component', class VanillaComponentElement extends HTMLElement { }, { })
+
+        // * Contains tests for the component. In normal web pages, this section will be
+        //      when the tag is downloaded. On a testing page, the tests are executed.
+        customElements.define('test-script', class TestScriptElement extends HTMLElement { }, { })
+
+        // * A component-markup tag contains HTML markup that defines a component. The component-markup tag can
+        // only appear inside a custom-component tag.
+        customElements.define('component-markup', class ComponentMarkupElement extends HTMLElement { }, { })
+
         // * Slots are loaded after all incude tags have been processed.
         // * component-slot appears inside a component. It's only attribute is an id. It marks a location that can
         // accept content from a slot-markup tag.
         customElements.define('component-slot', class ComponentMarkupElement extends HTMLElement { }, { })
+
         // * A slot-markup tag apears anywhere in the body of the page outside of a component. The slot-markup tag
         // has for-component-id and for-slot attributes that indicate which slot it's associated with. All children 
-        // elements of the slot-markup tag become children elements of the associated component-slot tag. Once this is
-        // done, the empty slot-markup tag is removed from the document.
+        // elements of the slot-markup tag become sibling elements of the associated component-slot tag. Once this is
+        // done, the empty slot-markup and the component-slot tags are removed from the document.
         // * The contents of a slot-markup tag are associated only with the DOM elements of an instance of a component.
         // They are not associated with the DOM fragment.
         customElements.define('slot-markup', class IncludeVarsElement extends HTMLElement { }, { })

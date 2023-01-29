6060ef2c61ca0d8ff5641c98e106bee40f809b3d
David Leeper <magicjtv@gmail.com>
2023-01-29 12:26:57 -0700

    Can now delete news items. Seperated client and server js code.

--- src/js/handlers/file.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-
-module.exports = (handlerName, handlerArgs) => {
-    return async (req, res, next) => {
-        var options = { root: path.join(`.`, '/src') }
-        res.sendFile(`${handlerArgs.file}`, options, (err) => { if (err) { next && next(err) } else { next && next() } })
-    }
-}
--- src/js/handlers/text.js
+++
@@ -1,6 +0,0 @@
-module.exports = (handlerName, handlerArgs) => {
-    return async (req, res, next) => {
-        res.send(handlerArgs.text)
-        next && next()
-    }
-}
